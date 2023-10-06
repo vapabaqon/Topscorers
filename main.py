d1767b2495b5385cef18cd9e0d98f61a414bdbe7
Jürg Käser <juerg.kaeser@abaqon.com>
2023-10-06 19:48:48 +0200

    limit functionality to login bonus only

--- main.py
+++ main.py
@@ -5,5 +5,3 @@ logger.setup_logger()
 
 jwt_token = acc.get_jwt_token()
 login_bonus = acc.get_login_bonus(jwt_token)
-account_overview = acc.get_account_overview(jwt_token)
-team_overview = acc.get_team_overview(jwt_token)
